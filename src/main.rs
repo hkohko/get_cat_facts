d7599746df5b1e1c5ab736f4d8e27c3933cc9820
heko <91366057+hkohko@users.noreply.github.com>
2023-11-14 18:26:11 +0700

    housekeeping

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,18 @@
 use hyper;
-use hyper::body::HttpBody;
-use hyper_tls;
-use hyper_tls::HttpsConnector;
-use hyper::client::HttpConnector;
+use hyper::http;
+use random_http_1::{build_client, build_req, process_req, ReqError};
 use tokio;
-use tokio::io::AsyncWriteExt;
+
 #[tokio::main]
-async fn main() {
-    let _ = sketch("https://catfact.ninja/fact", hyper::Method::GET).await;
+async fn main() -> Result<(), ReqError> {
+    let _ = get_cat_facts("https://catfact.ninja/fact", http::Method::GET).await?;
+    Ok(())
 }
-async fn build_req(uri: &hyper::Uri, method: hyper::Method) -> Result<hyper::Request<hyper::Body>, hyper::http::Error>{
-    let req = hyper::Request::builder()
-    .uri(uri)
-    .method(method)
-    .body(hyper::Body::empty());
-    req
-}
-async fn build_client() -> hyper::Client<HttpsConnector<HttpConnector>> {
-    let https_client = hyper::Client::builder().build::<_, hyper::Body>(hyper_tls::HttpsConnector::new());
-    https_client
-}
-async fn process_req(mut r: hyper::Response<hyper::Body>) {
-    while let Some(chunk) = r.body_mut().data().await {
-        tokio::io::stdout()
-            .write_all(&chunk.unwrap_or_default())
-            .await
-            .unwrap_or_default();
-    }
-}
-async fn sketch(url: &str, method: hyper::Method) {
+async fn get_cat_facts(url: &str, method: http::Method) -> Result<(), ReqError> {
     let uri: hyper::Uri = url.parse().unwrap_or_default();
-    let req = build_req(&uri, method).await.unwrap_or_default();
+    let req = build_req(&uri, method).await?;
     let https_client = build_client().await;
-    let r = https_client.request(req).await.unwrap_or_default();
+    let r = https_client.request(req).await?;
     process_req(r).await;
+    Ok(())
 }
